d0864e145c2eb122cc9587e01ae340b4bc364a44
Eady <edith.yelf@sky.uk>
2020-04-27 19:59:48 +0100

    interactingWithDomElementsContinued

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,9 @@ class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
   constructor() {
     // get access to elements and where they should be rendered
@@ -19,12 +22,38 @@ class ProjectInput {
     const importNode = document.importNode(this.templateElement.content, true);
     // render the content to the DOM
     this.element = importNode.firstElementChild as HTMLFormElement;
+    // access user-input from css file
+    this.element.id = "user-input";
     // call private method attach to make this execute
+    // populate fields in the constructor by using the querySelector on each form element to query for the form fields and cast as HTMLInputElement. This gives us access to all elements in objects created based on the class
+    this.titleInputElement = this.element.querySelector(
+      "#title"
+    ) as HTMLInputElement;
+    this.descriptionInputElement = this.element.querySelector(
+      "#description"
+    ) as HTMLInputElement;
+    this.peopleInputElement = this.element.querySelector(
+      "#people"
+    ) as HTMLInputElement;
+
+    // call methods from inside the class to ensure code runs
+    this.configure();
     this.attach();
   }
-  // private method added to split the seltion and rendering logic
-  //using insertAdjacentElement method to insert an element after the targeted elements opening 
-  // tags
+
+  // this method should trigger whenever the form is submitted
+  private submitHandler(event: Event) {
+    // prevent an http request being sent here
+    event.preventDefault();
+    console.log(this.titleInputElement.value);
+  }
+  // private method added to add listener element to form  and bind to private method
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
+  }
+
+  //    private method added to split the seltion and rendering logic
+  //   using insertAdjacentElement method to insert an element after the targeted elements opening tags
   private attach() {
     this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
