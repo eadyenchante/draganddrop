bd2e32883ea57a97aedc3b1c80a2e1b617c3173b
Eady <edith.yelf@sky.uk>
2020-04-28 18:54:58 +0100

    createReusableValidationFunctionality

--- src/app.ts
+++ src/app.ts
@@ -1,23 +1,23 @@
 // autobind decorator
 function autobind(
-    // hint for ts that we are not going to use the target and the methodName values but will accept them to use the argument thereafter
+  // hint for ts that we are not going to use the target and the methodName values but will accept them to use the argument thereafter
   _: any,
   _2: string,
   descriptor: PropertyDescriptor
 ) {
-    // access and store the original method that we defined
-    const originalMethod = descriptor.value;
-    const adjDescriptor: PropertyDescriptor = {
-        configurable: true,
-        // getter to call this on the original function and set to bndFn
-        // return tthe bound function
-       get(){
-        const boundFn = originalMethod.bind(this);
-        return boundFn;
-       }
-    };
-    // overall return the adjDescriptor method decorator
-    return adjDescriptor;
+  // access and store the original method that we defined
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    // getter to call this on the original function and set to bndFn
+    // return tthe bound function
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  // overall return the adjDescriptor method decorator
+  return adjDescriptor;
 }
 
 // project input class
@@ -63,12 +63,49 @@ class ProjectInput {
     this.configure();
     this.attach();
   }
+
+  // method to gather user input value and store in constants this will return a tuple or a union type to allow for potentially void 
+  private gatherUserInput(): [string, string, number] | void {
+    const enteredTitle = this.titleInputElement.value;
+    const enteredDescription = this.descriptionInputElement.value;
+    const enteredPeopleNo = this.peopleInputElement.value;
+
+    // validate TODO improve
+    // validation check to show alert or return tuple
+    if (
+      enteredTitle.trim().length === 0 ||
+      enteredDescription.trim().length === 0 ||
+      enteredPeopleNo.trim().length === 0
+    ) {
+alert('invalid input , please try again')
+    }else{
+        // convert enteredPeopleNo to
+        return [enteredTitle, enteredDescription, +enteredPeopleNo];
+    }
+  }
+
+  // method to clear input fields after submit
+  private  clearInputs() {
+      this.titleInputElement.value = '';
+      this.descriptionInputElement.value = '';
+      this.peopleInputElement.value = '';
+      
+  }
+
   @autobind
   // this method should trigger whenever the form is submitted
   private submitHandler(event: Event) {
     // prevent an http request being sent here
     event.preventDefault();
-    console.log(this.titleInputElement.value);
+    this.gatherUserInput();
+    // console.log(this.titleInputElement.value);
+    // if check using array method to establish if it returns an array and if true, destructuring to get the values out of userInput and log to console
+    const userInput = this.gatherUserInput();
+if(Array.isArray(userInput)){
+    const [title, desc, people] = userInput;
+    console.log(title, desc, people);
+    this.clearInputs();
+}
   }
   // private method added to add listener element to form  and bind to private method
   private configure() {
