44e50583ba7ba42cf6eb64fc251c778d6f67ea0f
Eady <edith.yelf@sky.uk>
2020-05-08 09:15:23 +0100

    namespaces

--- src/app.ts
+++ src/app.ts
@@ -1,30 +1,9 @@
+/// <reference path="drag-drop-interfaces.ts"/>
+/// <reference path="project-model.ts"/>
+
+namespace App {
 // Project Type
-enum ProjectStatus {
-  Active,
-  Finished,
-}
 
-// Drag and Drop Interfaces
-interface Draggable {
-  dragStartHandler(event: DragEvent): void;
-  dragEndHandler(event: DragEvent): void;
-}
-
-interface DragTarget {
-  dragOverHandler(event: DragEvent): void;
-  dropHandler(event: DragEvent): void;
-  dragLeaveHandler(event: DragEvent): void;
-}
-
-class Project {
-  constructor(
-    public id: string,
-    public title: string,
-    public description: string,
-    public people: number,
-    public status: ProjectStatus
-  ) {}
-}
 
 // Project State Management
 type Listener<T> = (items: T[]) => void;
@@ -370,6 +349,8 @@ class ProjectInput extends Component<HTMLDivElement, HTMLFormElement> {
   }
 }
 
-const prjInput = new ProjectInput();
-const activePrjList = new ProjectList("active");
-const finishedPrjList = new ProjectList("finished");
+new ProjectInput();
+new ProjectList("active");
+new ProjectList("finished");
+
+}
