23ef6165ce5074844366c72b7342d1c0bd5c7a9f
Eady <edith.yelf@sky.uk>
2020-05-22 14:11:23 +0100

    with error

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,26 @@
-import { ProjectInput } from './components/project-input';
+import { ProjectInput } from "./components/project-input";
 import { ProjectList } from "./components/project-list";
-import _ from 'lodash';
+import "reflect-metadata";
+import {Product} from "./product.model";
+import {plainToClass} from "class-transformer";
 
 new ProjectInput();
 new ProjectList("active");
 new ProjectList("finished");
 
-console.log(_.shuffle([1,2,3]));
+const products = [
+  { title: "a carpet", price: 29.99 },
+  { title: "a book", price: 10.99 },
+];
+
+// const loadedProducts = products.map((prod) => {
+//   return new Product(prod.title, prod.price);
+// });
+
+const loadedProducts = plainToClass(Product, products)
+
+for (const prod of loadedProducts) {
+  console.log(prod.getInfo());
+}
+
+// const p1 = new Product("a book,", 12.99);
